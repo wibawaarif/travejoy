adb2b27a38c25684fc45434fd85df44b37f17249
arif-phire <105232319+wibawaarif@users.noreply.github.com>
2023-02-21 21:37:22 +0700

    Edit scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import 'assets/scss/style.scss'
-
+import IconText from "parts/IconText";
 
 function App() {
   return (
     <div className="App">
       Hello world! 
+    <IconText></IconText>
     </div>
   );
 }
---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/Button/index'
+
+export default function IconText() {
+  return (
+    <Button className='brand-text-icon' href='' type='link'>
+        Trave<span className='text-grey-900'>joy</span>
+    </Button>
+  )
+}
