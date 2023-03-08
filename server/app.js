c3f2e9398a7b8b44d6d47e18715c04e025bff946
wibawaarif <arifwb.work@gmail.com>
2023-03-08 21:34:18 +0700

    add admin dashboard

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// router admin
+const adminRouter = require('./routes/admin')
 
 var app = express();
 
@@ -22,6 +24,7 @@ app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbo
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
