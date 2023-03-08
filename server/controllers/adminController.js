b768a5bf206e6b3b6cebfc7a2a4581b7a4871cd9
wibawaarif <arifwb.work@gmail.com>
2023-03-08 22:07:05 +0700

    Add view category ejs

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,5 +1,8 @@
 module.exports = {
     viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard');
+    },
+    viewCategory: (req, res) => {
+        res.render('admin/category/view_category');
     }
 }
