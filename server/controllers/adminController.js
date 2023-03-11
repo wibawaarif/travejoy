391c2e928e1948b1ece277fffe2bb167bc9e12b3
wibawaarif <arifwb.work@gmail.com>
2023-03-11 14:23:29 +0700

    Add show image item

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -150,6 +150,9 @@ module.exports = {
     },
     viewItem: async (req, res) => {
         try {
+            const item = await Item.find({})
+                .populate({path: 'imageId', select: 'id imageUrl'})
+                .populate({path: 'categoryId', select: 'id name'})
             const category = await Category.find({})
             const alertMessage = req.flash('alertMessage')
             const alertStatus = req.flash('alertStatus')
@@ -157,7 +160,9 @@ module.exports = {
             res.render('admin/item/view_item', {
                 title: "Travejoy | Item",
                 alert,
-                category
+                category,
+                item,
+                action: 'view'
             });
         } catch (error) {
             req.flash('alertMessage', `${error.message}`)
@@ -190,6 +195,28 @@ module.exports = {
             req.flash('alertStatus', 'success')
             res.redirect('/admin/item')
         }
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/admin/item')
+        }
+    },
+
+    showImageItem: async(req, res) => {
+        try {
+            const {id} = req.params;
+            const item = await Item.findOne({_id: id})
+                .populate({path: 'imageId', select: 'id imageUrl'})
+            const alertMessage = req.flash('alertMessage')
+            const alertStatus = req.flash('alertStatus')
+            const alert = {message: alertMessage, status: alertStatus}
+            res.render('admin/item/view_item', {
+                title: "Travejoy | Show Image Item",
+                alert,
+                item,
+                action: 'show image'
+            });
+            
         } catch (error) {
             req.flash('alertMessage', `${error.message}`)
             req.flash('alertStatus', 'danger')
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -20,6 +20,7 @@ router.delete('/bank/:id', adminController.deleteBank);
 // endpoint item
 router.get('/item', adminController.viewItem);
 router.post('/item', uploadMultiple, adminController.addItem);
+router.get('/item/show-image/:id', adminController.showImageItem);
 
 
 router.get('/booking', adminController.viewBooking);
