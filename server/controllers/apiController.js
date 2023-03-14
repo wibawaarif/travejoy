701432f9814e4daaead0bbb0b8d7b2226afca677
wibawaarif <arifwb.work@gmail.com>
2023-03-14 15:43:46 +0700

    Add detail page API data

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -2,6 +2,7 @@ const Item = require('../models/Item')
 const Treasure = require('../models/Activity')
 const Traveller = require('../models/Booking')
 const Category = require('../models/Category')
+const Bank = require('../models/Bank')
 
 module.exports = {
     landingPage: async(req, res) => {
@@ -65,5 +66,44 @@ module.exports = {
             
         }
 
+    },
+
+    detailPage: async (req, res) => {
+        try {
+            const {id} = req.params;
+            const item = await Item.findOne({_id: id})
+            .populate({
+                path: 'featureId',
+                select: '_id name qty imageUrl'
+            })
+            .populate({
+                path: 'activityId',
+                select: '_id name type imageUrl'
+            })
+            .populate({
+                path: 'imageId',
+                select: '_id imageUrl'
+            })
+
+            const bank = await Bank.find({})
+
+            const testimonial = {
+                _id: "asd1293uasdads1",
+                imageUrl: "/images/testimonial-landingpage.png",
+                name: "Happy Family",
+                rate: 4.55,
+                content: "What a great trip with my family and I should try again next trip soon ...",
+                familyName: "Arif",
+                familyOccupation: "Full Stack Developer"
+              }
+
+            res.status(200).json({
+                ...item._doc,
+                bank,
+                testimonial
+            })
+        } catch (error) {
+            
+        }
     }
 }
